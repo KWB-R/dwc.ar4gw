2e5c40db4a5a84239d81b2f4fe310524ee7122bb
Michael Rustler <michael.rustler@kompetenz-wasser.de>
2021-03-31 15:50:11 +0200

    Fix links to functions in "reticulate" package

--- R/import_flopy.R
+++ R/import_flopy.R
@@ -3,8 +3,8 @@
 #'
 #' @param convert Boolean (default: TRUE); should Python objects be automatically
 #' converted to their R equivalent? If set to FALSE, you can still manually convert
-#' Python objects to R via the \code{\link{reticulate::py_to_r}} function.
-#' @param ... additional arguments passed to \code{\link{reticulate::import}}
+#' Python objects to R via the \link[reticulate]{py_to_r} function.
+#' @param ... additional arguments passed to \link[reticulate]{import}
 #' @return imports "fl
 #' @export
 #' @importFrom reticulate import
