b9883be9261dca91a9b45765051474002473cf5b
Michael Rustler <michael.rustler@kompetenz-wasser.de>
2021-04-01 09:36:49 +0200

    download tutorial data from cloud and run on CI

--- R/import_flopy.R
+++ R/import_flopy.R
@@ -5,7 +5,7 @@
 #' converted to their R equivalent? If set to FALSE, you can still manually convert
 #' Python objects to R via the \link[reticulate]{py_to_r} function.
 #' @param ... additional arguments passed to \link[reticulate]{import}
-#' @return imports "fl
+#' @return imports "flopy" python package
 #' @export
 #' @importFrom reticulate import
 import_flopy <- function(convert = TRUE, ...) {
